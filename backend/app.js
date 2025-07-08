2bba91be6b4dd9580089f71868be8334c1c45d6b
RobbieLarios2024 <robbieavellana@gmail.com>
2025-07-08 14:04:57 -0600

    swagger

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,17 @@
 import express from "express";
 import clientRoutes from "./src/Routes/clients.js";
 import reservationRoutes from "./src/Routes/reservation.js";
+import swaggerUi from "swagger-ui-express";
+import fs from "fs";
+import path from "path";
 
 const app = express(); 
+
+const swaggerDocument = JSON.parse(
+  fs.readFileSync(path.resolve("./Documentacion.json"), "utf-8")
+);
+
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(express.json());
 app.use("/api/clients", clientRoutes);
